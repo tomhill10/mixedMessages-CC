9f96be84db04f9dbccc0d10d0af56aa6422dbce8
tomhill10 <tom@codehill.co.uk>
2023-08-07 19:28:09 +0100

    JS script and html/css done

---
+++ app.js
@@ -0,0 +1,48 @@
+console.log("this works");
+
+let subject = [];
+let verb = [];
+let object = [];
+
+async function getData() {
+    await fetch('./JSON/subject.JSON')
+        .then((response) => response.json())
+        .then(data => { subject = data; });
+
+    await fetch('./JSON/verb.JSON')
+        .then((response) => response.json())
+        .then((data) => { verb = data; });
+
+    await fetch('./JSON/object.JSON')
+        .then((response) => response.json())
+        .then((data) => { object = data; });
+}
+
+
+const button = document.getElementById("button");
+const quoteLocation = document.getElementById("quoteLocation");
+
+button.addEventListener("click", generateQuote);
+quoteLocation.addEventListener("click", generateQuote);
+
+function generateQuote() {
+    let intArray = [];
+    quoteLocation.classList.add("hide");
+    for (let i = 0; i < 3; i++) {
+        intArray.push(Math.floor(Math.random() * 30));
+    }
+
+    let quoteSubject = subject.subject[intArray[0]];
+    let quoteVerb = verb.verb[intArray[1]];
+    let quoteObject = object.object[intArray[2]];
+
+    let fullQuote = `${quoteSubject} ${quoteVerb} ${quoteObject}`;
+    console.log(fullQuote);
+    quoteLocation.textContent = fullQuote;
+    quoteLocation.classList.remove("hide");
+
+}
+
+
+getData();
+
